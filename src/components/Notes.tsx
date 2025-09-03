3e53b1f83c6e51666f2fddce3e637b9188085e7b
Julie Wang <juliewang617@gmail.com>
2025-09-03 10:44:44 -0400

    class 1

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -9,7 +9,8 @@ interface NoteData {
 const notesData: NoteData[] = [
   {
     date: "9/3: Welcome",
-    slidesUrl: "",
+    slidesUrl:
+      "https://docs.google.com/presentation/d/1skloaze3krgpx2QEQ52PFPLN1YTjJSyX/edit?usp=sharing&ouid=106249266650853110385&rtpof=true&sd=true",
     notesUrl: "",
   },
 ];
