1de980904651348bd606e603366d62ada8cc2bc2
Julie Wang <juliewang617@gmail.com>
2025-09-17 10:15:48 -0400

    lec 6

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -9,6 +9,12 @@ interface NoteData {
 
 const notesData: NoteData[] = [
   {
+    date: "9/15: Recipe, Semantics, and Recursion",
+    slidesUrl:
+      "https://docs.google.com/presentation/d/1XPp__n7BJNsELL6d5RwjSDLciEE15Yb3/edit?usp=sharing&ouid=106249266650853110385&rtpof=true&sd=true",
+    notesUrl: "",
+  },
+  {
     date: "9/12: Rules and The Design Recipe",
     slidesUrl:
       "https://docs.google.com/presentation/d/1TgTRPRmHWY0Z3AADt_hd1A7oGSKXQieh/edit?usp=drive_link&ouid=106249266650853110385&rtpof=true&sd=true",
