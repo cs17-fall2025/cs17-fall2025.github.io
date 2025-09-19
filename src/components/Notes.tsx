8404763ba5ee8dd2bcce4bfa9f7c5e52de974e91
Julie Wang <juliewang617@gmail.com>
2025-09-18 22:46:10 -0400

    lecture 7

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -9,6 +9,12 @@ interface NoteData {
 
 const notesData: NoteData[] = [
   {
+    date: "9/17: Lists and List Procedures",
+    slidesUrl:
+      "https://docs.google.com/presentation/d/1sRCmDc_7WXj-RW3IaLCBZadMUCfssq6A/edit?usp=sharing&ouid=106249266650853110385&rtpof=true&sd=true",
+    notesUrl: "",
+  },
+  {
     date: "9/15: Recipe, Semantics, and Recursion",
     slidesUrl:
       "https://docs.google.com/presentation/d/1XPp__n7BJNsELL6d5RwjSDLciEE15Yb3/edit?usp=sharing&ouid=106249266650853110385&rtpof=true&sd=true",
