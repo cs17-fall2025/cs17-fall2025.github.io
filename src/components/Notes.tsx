613207ff17181290ed783d4884e183785b1d8a6e
Julie Wang <juliewang617@gmail.com>
2025-10-15 11:03:20 -0400

    class 16

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -9,6 +9,11 @@ interface NoteData {
 
 const notesData: NoteData[] = [
   {
+    date: "10/15: Even more ReasonML",
+    slidesUrl:
+      "https://drive.google.com/file/d/1fMhPWbKnRITguvIXNt3oafvWQOScGWWw/view?usp=sharing",
+  },
+  {
     date: "10/10: More ReasonML",
     slidesUrl:
       "https://drive.google.com/file/d/1R6sy2TYgCop91xt1LOK_HZ7A3u0v2DPf/view?usp=sharing",
