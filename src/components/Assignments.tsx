421fa0c3f27a820dddf9b6aeb2f8ea5c1e746153
Julie Wang <juliewang617@gmail.com>
2025-10-16 14:55:04 -0400

    hw7

--- src/components/Assignments.tsx
+++ src/components/Assignments.tsx
@@ -142,14 +142,15 @@ const homeworks = [
   //   link: "",
   //   source: "",
   // },
-  // {
-  //   title: "HW 7: Rackettecita",
-  //   out: new Date("17 Oct 2025 12:00:00 EST"),
-  //   due: new Date("23 Oct 2025 22:00:00 EST"),
-  //   late: new Date("24 Oct 2025 22:00:00 EST"),
-  //   link: "",
-  //   source: "",
-  // },
+  {
+    title: "HW 7: Rackettecita",
+    out: new Date("16 Oct 2025 00:00:00 EST"),
+    due: new Date("22 Oct 2025 22:00:00 EST"),
+    late: new Date("24 Oct 2025 22:00:00 EST"),
+    link: "https://drive.google.com/file/d/11AXw3jU6HoE5wL5Lny4EMGsbOR6yR-An/view?usp=sharing",
+    source:
+      "https://drive.google.com/drive/folders/11sqyVh7cLpYZlYJpTfngIZKHRtvyXhyQ?usp=sharing",
+  },
   {
     title: "HW 6: K-Subsets and Matrices",
     out: new Date("10 Oct 2025 00:00:00 EST"),
