b1c0c40eb43fce4c3135b539bcf2a6cb7294277d
skylarwalters <skylar_walters@brown.edu>
2025-10-30 10:29:53 -0400

    hw9

--- src/components/Assignments.tsx
+++ src/components/Assignments.tsx
@@ -132,9 +132,9 @@ const homeworks = [
   // },
   {
     title: "HW 9: Trees, Trees, ...and Lists",
-    out: new Date("7 Nov 2025 12:00:00 EST"),
-    due: new Date("13 Nov 2025 22:00:00 EST"),
-    late: new Date("14 Nov 2025 22:00:00 EST"),
+    out: new Date("30 Oct 2025 01:00:00 EST"),
+    due: new Date("5 Nov 2025 22:00:00 EST"),
+    late: new Date("6 Nov 2025 22:00:00 EST"),
     link: "https://drive.google.com/file/d/1EGemfD_y8I0NLg_3RQaKPH640ndPVQaC/view?usp=sharing",
     source: "https://drive.google.com/drive/folders/1lstyztXudlXt8RHiS1jKHQ1nlYrEjLR2?usp=sharing",
   },
